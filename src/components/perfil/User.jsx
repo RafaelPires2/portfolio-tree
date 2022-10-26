3ee80e6c0dee2c8166c8616e1cba46817c86fa6d
RafaelPires2 <rafaelbrpires@gmail.com>
2022-10-26 15:29:14 -0300

    img

--- src/components/perfil/User.jsx
+++ src/components/perfil/User.jsx
@@ -3,7 +3,7 @@ import styles from './User.module.css';
 export function User() {
     return (
         <>
-            <img className={styles.user} src='./src/assets/rafaelpires-perfil.png' />
+            <img className={styles.user} src="./src/assets/rafaelpires-perfil.png" />
             <h2 className={styles.title}>Rafael Pires</h2>
             <p className={styles.subtitle}>Ciclo da Riqueza</p>
         </>
