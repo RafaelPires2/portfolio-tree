ecc31161ac47d47900b69aa3757123e655814ee3
RafaelPires2 <rafaelbrpires@gmail.com>
2022-10-26 14:36:07 -0300

    background

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ export function App() {
 
   return (
     <>
+    <div className={global.container}>
     <div className={global.wrapper}>
     <User /> 
     <Post titulo="Ebook: Ciclo da Riqueza" link="https://www.missaocactus.com.br"/>
-    <Post titulo="Youtube" link="https://www.missaocactus.com.br"/>
-    <Post titulo="Instagram" link="https://www.missaocactus.com.br"/>
-    <Post titulo="TikTok" link="https://www.missaocactus.com.br"/>
+    <Post titulo="Youtube" link="https://www.youtube.com/channel/UCz1aiyLv8ndOrfpQi5qwoJw"/>
+    <Post titulo="Instagram" link="https://www.instagram.com/rafaelbrpires/"/>
+    <Post titulo="TikTok" link="https://www.tiktok.com/@rafaelbrpires2?is_from_webapp=1&sender_device=pc"/>
     <Post titulo="Grupo Exclusivo Telegram" link="https://www.missaocactus.com.br"/>
+    </div>
     </div>
     </>
   )
