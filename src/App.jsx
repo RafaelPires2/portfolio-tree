2163237e08dbcd0eb306e3ce9371a5a5ded8c392
RafaelPires2 <rafaelbrpires@gmail.com>
2022-11-04 10:55:13 -0300

    ref links

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export function App() {
     <div className={global.container}>
     <div className={global.wrapper}>
     <User /> 
-    <Post titulo="Ebook: Ciclo da Riqueza" link="https://www.missaocactus.com.br"/>
+    <Post titulo="Ebook: Ciclo da Riqueza" link="https://hotm.art/YZfLgzn"/>
     <Post titulo="Youtube" link="https://www.youtube.com/channel/UCz1aiyLv8ndOrfpQi5qwoJw"/>
     <Post titulo="Instagram" link="https://www.instagram.com/rafaelbrpires/"/>
     <Post titulo="TikTok" link="https://www.tiktok.com/@rafaelbrpires2?is_from_webapp=1&sender_device=pc"/>
